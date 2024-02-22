d33429773eb854738266ebc43186e1e0d2a3ab94
Shepardinio <mel-yand@student.42.fr>
2024-02-22 17:19:36 +0100

    toujours rien fait

--- src/parsing.c
+++ src/parsing.c
@@ -1,29 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   parsing.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mel-yand <mel-yand@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/20 21:31:47 by mel-yand          #+#    #+#             */
-/*   Updated: 2024/02/22 16:33:14 by mel-yand         ###   ########.fr       */
+/*   Created: 2024/02/22 16:55:08 by mel-yand          #+#    #+#             */
+/*   Updated: 2024/02/22 16:56:54 by mel-yand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./libft.h"
 #include "./so_long.h"
 
-void	init_struct(t_data *data)
+void	ft_parsing(t_data *data, char *argv)
 {
-	data->map = NULL;
-}
-
-int	main(int argc, char **argv)
-{
-	t_data *data;
-
-	if (argc != 2)
-		return (printf("to mut"));
-	init_data(&data);
-	ft_parsing(&data, argv[1]);
+	
 }
